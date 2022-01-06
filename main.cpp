30a8c5c1f53b72480e2d102cbe0289049abea70f
yuuki1293 <yuuki1293@outlook.jp>
2022-01-06 23:06:36 +0900

    MSCoreクラスが一応完成

--- main.cpp
+++ main.cpp
@@ -10,7 +10,43 @@ using namespace std;
 
 int main()
 {
-    TestForm* test_scene = new TestForm(new Weggx(WindowInfo{24,500,500}));
-    delete test_scene;
-    while(1);
+    int row,col,bomb;
+    printf("縦 横 爆弾の数の順に入力してね\n");
+    scanf("%d%d%d",&row,&col,&bomb);
+    MSCore new_ms(row,col,bomb);
+    while(1){
+        printf("縦 横の順に入力してね\n");
+        scanf("%d%d",&row,&col);
+        int result = new_ms.click(posi{row,col});
+        switch (result)
+        {
+        case -4:
+            printf("範囲外だよ!\n");
+            break;
+        case -3:
+            printf("もう開いてるよ!\n");
+            break;
+        case -2:
+            printf("旗が立ってるよ!\n");
+            break;
+        case -1:
+            printf("爆発したよ!\n");
+            break;
+        default:
+            printf("(%d, %d) の数字は %d だよ!\n",row,col,result);
+            break;
+        }
+        switch (new_ms.get_status())
+        {
+        case 1:
+            printf("ゲームオーバー\n");
+            std::exit(0);
+            break;
+        case 2:
+            printf("ゲームクリア\n");
+            std::exit(0);
+        default:
+            break;
+        }
+    }
 }
--- minesweeper_core.hpp
+++ minesweeper_core.hpp
@@ -10,7 +10,7 @@ struct posi
 {
     int row;
     int column;
-    bool is_neighbor(posi r) // NOTE 使わないかもしれない
+    bool is_neighbor(posi r)
     {
         int row_dist = abs(r.row - row);
         int colum_dist = abs(r.column - column);
@@ -29,15 +29,19 @@ private:
     std::vector<posi> bombs;
     std::vector<posi> flags;
     std::vector<std::vector<bool>> opened;
+    int opened_count=0;
     int cells_count() { return row * column; }
     int row, column;
     std::random_device seed_gen;
     // 0:プレイ中 1:ゲームオーバー 2:クリア
     int status;
+
 public:
-    // -3:もう開いてるよ -2:旗だよ -1:爆弾だよ 0~:周りの爆弾の数だよ
+    // -4:範囲外だよ -3:もう開いてるよ -2:旗だよ -1:爆弾だよ 0~:周りの爆弾の数だよ
     int click(posi cell)
     {
+        if (cell.row >= row || cell.column >= column)
+            return -4;
         if (opened[cell.row][cell.column])
             return -3;
         for (size_t i = 0; i < flags.size(); i++)
@@ -45,25 +49,38 @@ public:
                 return -2;
         for (size_t i = 0; i < bombs.size(); i++)
             if (bombs[i] == cell)
+            {
+                status = 1;
                 return -1;
+            }
         opened[cell.row][cell.column] = true;
+        opened_count++;
+        if (opened_count >= cells_count() - (int)bombs.size())
+            status = 2;
         int neighbor_boms_count = 0;
         for (size_t i = 0; i < bombs.size(); i++)
             if (cell.is_neighbor(bombs[i]))
                 neighbor_boms_count++;
         return neighbor_boms_count;
     }
+    // 0:プレイ中 1:ゲームオーバー 2:クリア
+    int get_status() { return status; }
     MSCore(int, int, int);
     ~MSCore();
 };
 
-MSCore::MSCore(int column, int row, int bomb_num)
+MSCore::MSCore(int row, int column, int bomb_num)
 {
     if (row * column <= bomb_num)
         throw std::runtime_error("爆弾の数がセルの数と同じかそれより多いよ!");
 
     this->row = row;
     this->column = column;
+    bombs.resize(bomb_num);
+    opened.resize(row);
+    for (int i = 0; i < row; i++)
+        opened[i].resize(column);
+
     std::random_device rd;
     std::default_random_engine engine(seed_gen());
 
